08192036a6c415cb8ab795644e6c2a26574b0c19
Pavel Chesnokov <p.chesnokov@gmail.com>
2019-11-18 17:18:29 +0300

    fix

--- src/blocks/modules/request/request.js
+++ src/blocks/modules/request/request.js
@@ -1,3 +1,7 @@
 $( document ).ready(function() {
-
+    // $('.button-send__modal').click(
+    //     function () {
+    //         $('body').addClass('overflow-hidden');
+    //     }
+    // )
 });
