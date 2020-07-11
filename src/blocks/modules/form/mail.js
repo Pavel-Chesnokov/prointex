140acd1d9c45badd37d077a4631cdf9913c15e37
Pavel Chesnokov <p.chesnokov@gmail.com>
2020-07-11 16:36:04 +0300

    capcha create

--- src/blocks/modules/form/mail.js
+++ src/blocks/modules/form/mail.js
@@ -1,27 +1,42 @@
-// Отправка данных на сервер
-"use strict";
-$(".form").trigger("reset");
-$(function() {
-    $(".form").on("submit", function(e) {
-        e.preventDefault();
-        $.ajax({
-            url: "https://prointechnology.ru/wp-content/themes/prointex/send.php",
-            //url: "//localhost:4000/wordpress/wp-content/themes/prointex/send.php", //работает на локальном сервере
-            type: "POST",
-            contentType: false,
-            processData: false,
-            data: new FormData(this),
-            success: function(msg) {
-                if (msg == "ok") {
-                    $(".form").trigger("reset"); // очистка формы
-                    $(".close").parents(".popup-fade");
-                    $(".popup-fade").removeClass("popup-fade-show");
-                    $(".popup-fade-secces").addClass("popup-fade-show");
-                    // alert("Сообщение отправлено");
-                } else {
-                    alert("Ошибка");
+$('#form').submit(function(event) {
+    event.preventDefault(); // Prevent direct form submission
+    $('#alert').text('Processing...').fadeIn(0); // Display "Processing" to let the user know that the form is being submitted
+    grecaptcha.ready(function () {
+        console.log('i am redy ');
+        grecaptcha.execute('6Leoa68ZAAAAAFhrhHXVIqwkRq9HMQ_acuTY9ZbG', { action: 'contact' }).then(function (token) {
+            var recaptchaResponse = document.getElementById('recaptchaResponse');
+            recaptchaResponse.value = token;
+            $.ajax({
+                url: 'https://prointechnology.ru/wp-content/themes/prointex/mailsend.php',
+                type: 'post',
+                data: $('#form').serialize(),
+                dataType: 'json',
+                success: function( _response ){
+                    // The Ajax request is a success. _response is a JSON object
+                    var error = _response.error;
+                    var success = _response.success;
+                    if(error != "") {
+                        // In case of error, display it to user
+                        $('#alert').html(error);
+                    }
+                    else {
+                        $(".form").trigger("reset"); // очистка формы
+                        $(".close").parents(".popup-fade");
+                        $(".popup-fade").removeClass("popup-fade-show");
+                        $(".popup-fade-secces").addClass("popup-fade-show");
+                        // In case of success, display it to user and remove the submit button
+                        $('#alert').html(success);
+                        // $('#submit-button').remove();
+                    }
+                },
+                error: function(jqXhr, json, errorThrown){
+                    // In case of Ajax error too, display the result
+                    var error = jqXhr.responseText;
+                    $('#alert').html(error);
                 }
-            }
+            });
         });
     });
 });
+
+
--- src/php/functions.php
+++ src/php/functions.php
@@ -5,17 +5,31 @@ add_action( 'wp_enqueue_scripts', 'theme_name_scripts' );
 function theme_name_scripts() {
 	wp_enqueue_style( 'style-name', get_template_directory_uri() . '/style.css');
 	// wp_enqueue_style( 'style-name', get_stylesheet_uri() );
+	// wp_deregister_script( 'jquery' );
+	// wp_register_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js', false, null, true );
+	// wp_enqueue_script( 'jquery' );
 	wp_deregister_script( 'jquery' );
-	wp_register_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js', false, null, true );
+	wp_register_script( 'jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js', false, null, true );
 	wp_enqueue_script( 'jquery' );
 	wp_enqueue_script( 'TweenMax-name', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js', array(), 'null', true );
 	wp_enqueue_script( 'TimelineMax-name', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TimelineMax.min.js', array(), 'null', true );
 	wp_enqueue_script( 'vendor-name', get_template_directory_uri() . '/js/vendor.js', array(), 'null', true );
 	wp_enqueue_script( 'main-name', get_template_directory_uri() . '/js/main.js', array(), 'null', true );
 	wp_enqueue_script( 'map-name', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAzepC77rzIAjEVqeiCpjm__5fP0MeF6-4&callback=initMap', array(), 'null', true );
-
+	wp_enqueue_script( 'capcha-name', 'https://www.google.com/recaptcha/api.js?render=6LfsKrAZAAAAALprlSd7jfX-pwjdxTKnrglHrPRo', array(), 'null', false );
 
 }
 
 
 
+function add_defer_attribute($tag, $handle) {
+	// add script handles to the array below
+	$scripts_to_defer = array('map-name', 'capcha-name');
+	foreach($scripts_to_defer as $defer_script) {
+	   if ($defer_script === $handle) {
+		  return str_replace(' src', ' async="async" src', $tag);
+	   }
+	}
+	return $tag;
+ }
+ add_filter('script_loader_tag', 'add_defer_attribute', 10, 2);
