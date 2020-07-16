3e3da64821ae3d763427229e552792591872d379
Pavel Chesnokov <p.chesnokov@gmail.com>
2020-07-17 01:04:45 +0300

    lazy load google map created

--- src/php/functions.php
+++ src/php/functions.php
@@ -15,7 +15,7 @@ function theme_name_scripts() {
 	wp_enqueue_script( 'TimelineMax-name', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TimelineMax.min.js', array(), 'null', true );
 	wp_enqueue_script( 'vendor-name', get_template_directory_uri() . '/js/vendor.js', array(), 'null', true );
 	wp_enqueue_script( 'main-name', get_template_directory_uri() . '/js/main.js', array(), 'null', true );
-	wp_enqueue_script( 'map-name', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAzepC77rzIAjEVqeiCpjm__5fP0MeF6-4&callback=initMap', array(), 'null', true );
+	//wp_enqueue_script( 'map-name', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAzepC77rzIAjEVqeiCpjm__5fP0MeF6-4&callback=initMap', array(), 'null', true );
 	wp_enqueue_script( 'capcha-name', 'https://www.google.com/recaptcha/api.js?render=6Leoa68ZAAAAAFhrhHXVIqwkRq9HMQ_acuTY9ZbG', array(), 'null', false );
 	//wp_enqueue_script( 'capcha-name', 'https://www.google.com/recaptcha/api.js?render=6LfsKrAZAAAAALprlSd7jfX-pwjdxTKnrglHrPRo', array(), 'null', false );
 
